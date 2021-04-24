fcffe6fb673e45c1048ffd417ca87e13058d9ef6
Joe-Joy <71425992+Joe-Joy@users.noreply.github.com>
2021-04-24 18:11:44 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("supermarket application designed");
+  res.send("supermarket_two application designed");
 });
 
 module.exports = router;
