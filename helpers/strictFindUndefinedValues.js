8250ea5de959312e61f09f4620ab11b6f8a7c7b9
fourtimes <jjino@protonmail.com>
2021-04-25 21:52:06 +0530

    spelling modified

--- helpers/strictFindUndefinedValues.js
+++ helpers/strictFindUndefinedValues.js
@@ -1,4 +1,4 @@
-module.exports.strictFindUndefinedValues = (param) => {
+const strictFindUndefinedValues = (param) => {
   var params = Object.entries(param);
   let len = params.length;
   let undefinedVariable = [];
@@ -12,3 +12,22 @@ module.exports.strictFindUndefinedValues = (param) => {
   }
   return undefinedVariable;
 };
+
+const nonStrictfindUndefinedValues = (param) => {
+  var params = Object.entries(param);
+  let len = params.length;
+  let undefinedVariable = [];
+  for (let i = 0; i < len; i++) {
+    let values = params[i];
+    var a = values.indexOf(undefined);
+    if (a === 1 )  {
+      undefinedVariable.push(values[0]);
+    }
+  }
+  return undefinedVariable;
+};
+
+module.exports = {
+  strictFindUndefinedValues,
+  nonStrictfindUndefinedValues
+}
