773be3b582af7ad726d12f5e292c20f1d12d0f4d
fourtimes <jjino@protonmail.com>
2021-04-25 15:14:49 +0530

    controller section added

---
+++ controller/controller_item_details.js
@@ -0,0 +1,36 @@
+const writesql = require("../config/writeSql");
+const readSql = require("../config/readSql");
+const fuv = require("../helpers/findUndefinedValues");
+
+exports.get_item_details = (req, res) => {
+  res.send({ success: true, message: "items inserted!" });
+};
+
+
+exports.get_item_details_data = (req, res) => {
+  var query = `SELECT * FROM items_details`;
+  readSql.query(query, (error, results, fields) => {
+    return error == "" ? res.send({ success: true, message: "items inserted!", results }) : res.status(400).json({ success: false, message: error.code.toLowerCase() });
+  });
+}
+
+
+exports.get_item_details_insert = (req, res) => {
+  let data = req.body;
+  let rData = {
+    items_code: data.items_code,
+    items_name: data.items_name,
+    variety_name: data.variety_name,
+  };
+
+  finalDatas = fuv.findUndefinedValues(rData);
+  
+  if (finalDatas == "") {
+    var query = `INSERT INTO items_details(items_code,items_name,variety_name) VALUES ('${rData.items_code}','${rData.items_name}','${rData.variety_name}')`;
+    writesql.query(query, (error, results, fields) => {
+      return  error == "" ? res.send({ success: true, message: "items inserted!", results }) : res.status(400).json({ success: false, message: error.code });
+    });
+  } else {
+      return res.status(400).json({ success: false, message: "order not found!" });
+  }
+}
--- routes/items_details.js
+++ routes/items_details.js
@@ -1,39 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const writesql = require("../config/writeSql");
-const readSql = require("../config/readSql");
-const fuv = require("../helpers/findUndefinedValues");
 
+const controller_item_details = require("../controller/controller_item_details");
 
-router.get("/", (req, res) => {
-  res.send({ success: true, message: "items inserted!" })
-});
+router.get("/", controller_item_details.get_item_details);
 
-router.get("/data", (req, res) => {
-  var query = `SELECT * FROM items_details`;
-  readSql.query(query, (error, results, fields) => {
-    return error == "" ? res.send({ success: true, message: "items inserted!", results }) : res.status(400).json({ success: false, message: error.code.toLowerCase() });
-  });
-});
+router.get("/data", controller_item_details.get_item_details_data);
 
-router.post("/insert", (req, res) => {
-  let data = req.body;
-  let undefinedDatas = {
-    items_code: data.items_code,
-    items_name: data.items_name,
-    variety_name: data.variety_name,
-  };
-
-  finalDatas = fuv.findUndefinedValues(undefinedDatas);
-  
-  if (finalDatas == "") {
-    var query = `INSERT INTO items_details(items_code,items_name,variety_name) VALUES ('${undefinedDatas.items_code}','${undefinedDatas.items_name}','${undefinedDatas.variety_name}')`;
-    writesql.query(query, (error, results, fields) => {
-      return  error == "" ? res.send({ success: true, message: "items inserted!", results }) : res.status(400).json({ success: false, message: error.code });
-    });
-  } else {
-      return res.status(400).json({ success: false, message: "order not found!" });
-  }
-});
+router.post("/insert", controller_item_details.get_item_details_insert);
 
 module.exports = router;
