72bc46ae2b6cc01b6998705aa8d39a58d8286197
fourtimes <57703276+FourTimes@users.noreply.github.com>
2021-04-26 18:50:58 +0530

    Update route_item_uploads.js

--- routes/route_item_uploads.js
+++ routes/route_item_uploads.js
@@ -1,1 +1,8 @@
 const express = require("express");
+const router = express.Router();
+const uploadController = require("../controller/controller_item_uploads");
+
+router.get("/", controller_item_sales.get_item_sales_root);
+router.post("/multiple-upload",  uploadController.uploadImages,  uploadController.resizeImages,  uploadController.getResult);
+
+module.exports = router;
